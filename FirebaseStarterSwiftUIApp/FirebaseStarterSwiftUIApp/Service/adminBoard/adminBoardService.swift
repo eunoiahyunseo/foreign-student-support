89cba0068a0b1103a23f6f96db7949a9e172de82
Park Jun ha <wns7756@naver.com>
2023-06-17 06:40:59 +0900

    adminBoard CREATE function

---
+++ FirebaseStarterSwiftUIApp/FirebaseStarterSwiftUIApp/Service/adminBoard/adminBoardService.swift
@@ -0,0 +1,9 @@
+//
+//  adminBoardService.swift
+//  FirebaseStarterSwiftUIApp
+//
+//  Created by junha on 2023/06/17.
+//  Copyright © 2023 iOS App Templates. All rights reserved.
+//
+
+import Foundation
